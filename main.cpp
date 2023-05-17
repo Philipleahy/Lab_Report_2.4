c4e498c5f85f85051bda27bc873faf55c76967bd
Philipleahy <philip.leahy@HOTMAIL.COM>
2023-05-17 20:23:49 +0000

    Second Commit - Verified as working

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
+DigitalIn fire(p14);
+PwmOut spkr(p26);
+AnalogIn pot1(p19);
 int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+ while (1) {
+ for (float i=2000.0; i<10000.0; i+=100) {
+ spkr.period(1.0/i);
+ spkr=0.5;
+ wait(0.1);
+ }
+ spkr=0.0;
+ while(pot1.read() < 0.5) {} // this uses the pot to control the program
+ }
 }
+
