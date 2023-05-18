ef3e68d2a37831e08bc0f07edd649c2aee9ca070
Philipleahy <philip.leahy@HOTMAIL.COM>
2023-05-18 16:23:48 +0000

    Fibal Commit

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@ int main() {
  for (float i=2000.0; i<10000.0; i+=100) {
  spkr.period(1.0/i);
  spkr=0.5;
- wait(0.1);
- }
- {for (float i=4000.0; i<20000.0; i+=200) {
- spkr.period(1.0/i);
- spkr=0.5;
- wait(0.1);
- }
+ wait(2);
+}
+{for (float i=1000.0; i<5000.0; i+=50) {
+spkr.period(1.0/i);
+spkr=0.5;
+wait(2);
+}
  spkr=0.0;
  while(pot1.read() < 0.5) {} // this uses the pot to control the program
  }
