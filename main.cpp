76ab8dc3a6dd7223a8002ec9477e0a55702658d9
Philipleahy <philip.leahy@HOTMAIL.COM>
2023-05-18 16:56:55 +0000

    Complete

--- main.cpp
+++ main.cpp
@@ -8,15 +8,18 @@ AnalogIn pot1(p19);
 
 int main() {
  while (1) {
- for (float i=2000.0; i<10000.0; i+=100) {
- spkr.period(1.0/i);
- spkr=0.5;
- wait(2);
-}
-{for (float i=1000.0; i<5000.0; i+=50) {
+for (float i=2000.0; i<10000.0; i+=100) {
 spkr.period(1.0/i);
 spkr=0.5;
-wait(2);
+wait(.2);
+}
+
+{for (float i=2000; i<5000; i+=100) {
+spkr.period(1.0/i);
+spkr=0.25;
+wait(0.02);
+spkr=0.0;
+wait(0.5);
 }
  spkr=0.0;
  while(pot1.read() < 0.5) {} // this uses the pot to control the program
